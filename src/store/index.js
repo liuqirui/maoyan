7c2bfac2745525d0dee7163d6f293173991b1d0d
tanliang <575791615@qq.com>
2017-09-08 15:57:35 +0800

    提交影厅

--- src/store/index.js
+++ src/store/index.js
@@ -5,12 +5,14 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 //影院管理的store
 import Cia from "./modules/cinemastore.js"
+import screens from "./modules/screens.js" //放映厅的 store -谭亮
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
     modules: {
         cia:Cia,
+        screens
     }
 })
 export default store;
