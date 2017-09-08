1cf2786cee01249d45e208b32277061dae62d83a
zhaoyingbo <754476428@qq.com>
2017-09-08 15:18:35 +0800

    上传影院管理

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,14 @@ import axios from 'axios';
 
 import Vue from 'vue';
 import Vuex from 'vuex';
-
+//影院管理的store
+import Cia from "./modules/cinemastore.js"
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
     modules: {
-        
+        cia:Cia,
     }
 })
 export default store;
