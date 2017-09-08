45a6b1e7c0afc6020c4972970f84380eaf70e9d6
zhangxiao <402574700@qq.com>
2017-09-08 17:33:11 +0800

    修改了请求路径

--- src/store/index.js
+++ src/store/index.js
@@ -2,26 +2,26 @@ import axios from 'axios';
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-<<<<<<< HEAD
+
 import Match from './modules/match'
-=======
+
 import Movie from './modules/movie'
 
 import Cia from "./modules/cinemastore.js"
 import screens from "./modules/screens.js" 
->>>>>>> 41389faf4101e58ed33e839f7a8723d6427d7879
+
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
     modules: {
-<<<<<<< HEAD
-       match:Match
-=======
+
+       match:Match,
+
         movie:Movie,
         cia:Cia,
         screens
->>>>>>> 41389faf4101e58ed33e839f7a8723d6427d7879
+
     }
 })
 export default store;
